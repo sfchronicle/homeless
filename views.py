75e4fc0658213881b0837c17f848f66af8a643dc
Lucio Villa <lucio310@gmail.com>
2016-06-01 12:20:19 -0700

    installed pym.js

--- views.py
+++ views.py
@@ -13,3 +13,94 @@ def index():
     	description='',
     	twitter_text='',
     	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/setup/')
+def setup():
+    return render_template('setup.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/insight/')
+def insight():
+    return render_template('insight.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/photos/')
+def photos():
+    return render_template('photos.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/data/')
+def data():
+    return render_template('data.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/shelters/')
+def shelters():
+    return render_template('shelters.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/mental/')
+def mental():
+    return render_template('mental.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/camping/')
+def camping():
+    return render_template('camping.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/supportive/')
+def supportive():
+    return render_template('supportive.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
+@app.route('/opinion/')
+def opinion():
+    return render_template('opinion.html',
+    	slug='',
+    	social_img='',
+    	social_title='',
+    	description='',
+    	twitter_text='',
+    	email_text='mailto:?subject=Subject&body=Body')
+
